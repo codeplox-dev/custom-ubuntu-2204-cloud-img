853be6303ade98abea8c9d1ae6206a15cbf51445
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-10-16 18:39:34 -0400

    fix: shrink size

--- install_packer_and_build.sh
+++ install_packer_and_build.sh
@@ -172,8 +172,8 @@ main(){
     PACKER_LOG=1 ${SUDO}packer build ubu2204_jammy.pkr.hcl
 
     ${SUDO}qemu-img convert -O qcow2 ${OUT_DIR}/${IMG} ${OUTPUT_QCOW}
-    ${SUDO}qemu-img resize -f qcow2 ${OUTPUT_QCOW} 32G
-    ${SUDO}virt-sparsify --in-place ${OUTPUT_QCOW}
+    ${SUDO}qemu-img resize -f qcow2 ${OUTPUT_QCOW} 8G
+    #${SUDO}virt-sparsify --in-place ${OUTPUT_QCOW}
     ${SUDO}virt-sparsify --compress ${OUTPUT_QCOW} ${OUTPUT_QCOW_COMPRESSED}
     echo -e "\n\n==> FINISHED!! Output image: ${OUTPUT_QCOW_COMPRESSED}"
 
